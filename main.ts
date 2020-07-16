42dd13d97ca604965e4eaf6479e25f758d306b0d
Davnox900 <68035597+Davnox900@users.noreply.github.com>
2020-07-16 13:40:34 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,8 @@ input.onButtonPressed(Button.A, function () {
     if (X > 0) {
         led.unplot(X, Y)
         X += -1
-    } else if (X < 0 && Y == 0) {
+    } else if (X == 4 && Y == -1) {
+        led.unplot(X, Y)
         Y = 4
         Y = 4
     } else {
@@ -15,7 +16,8 @@ input.onButtonPressed(Button.B, function () {
     if (X < 4) {
         led.unplot(X, Y)
         X += 1
-    } else if (X < 4 && Y == 4) {
+    } else if (X == 0 && Y == 5) {
+        led.unplot(X, Y)
         X = 0
         Y = 0
     } else {
@@ -27,7 +29,6 @@ input.onButtonPressed(Button.B, function () {
 let Y = 0
 let X = 0
 X = 0
-led.plot(X, 0)
 Y = 0
 basic.forever(function () {
     led.plot(X, Y)
